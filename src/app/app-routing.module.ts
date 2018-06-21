8280cea3af76e4cb964d4584020e450db18c12e2
CHANDRA-PC\CHANDRA <tchandu.soft@gmail.com>
2018-06-21 11:53:09 +0530

    Shared Modules added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { CalendarComponent } from './shared/calendar/calendar.component';
+import { ProfileComponent } from './shared/profile/profile.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'calendar', component: CalendarComponent
+  },
+  {
+    path: 'profile', component: ProfileComponent
+  },
+  {
+    path: '**', component: PagenotfoundComponent, pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CalendarModule } from 'angular-calendar';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContentComponent } from './shared/content/content.component';
+import { HomeComponent } from './shared/home/home.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { CalendarComponent } from './shared/calendar/calendar.component';
+import { ProfileComponent } from './shared/profile/profile.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    HomeComponent,
+    DashboardComponent,
+    CalendarComponent,
+    ProfileComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule, FormsModule, ReactiveFormsModule,
+    AppRoutingModule, NgbModule.forRoot(), CalendarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
